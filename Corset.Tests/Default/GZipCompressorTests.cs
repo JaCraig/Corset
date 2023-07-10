847e32dfbedfccbc4e0b1e4c8487abef761bcb17
JaCraig <craig.jamesa@gmail.com>
2023-07-10 16:02:22 -0400

    fix: Lets try this again

--- Corset.Tests/Default/GZipCompressorTests.cs
+++ Corset.Tests/Default/GZipCompressorTests.cs
@@ -14,7 +14,7 @@ namespace Corset.Tests.Default
             var TestObject = new GZipCompressor();
             const string Data = "This is a bit of data that I want to compress";
             var Result = Convert.ToBase64String(TestObject.Compress(Data.ToByteArray()));
-            Assert.True("H4sIAAAAAAAACgrJyCxWAKJEhaTMEoX8NIWUxJJEhZKMxBIFT4XyxLwShZJ8heT83IKi1OJiAAAAAP//" == Result || "H4sIAAAAAAAAAgrJyCxWAKJEhaTMEoX8NIWUxJJEhZKMxBIFT4XyxLwShZJ8heT83IKi1OJiAAAAAP//" == Result);
+            Assert.Contains(Result, new string[] { "H4sIAAAAAAAACgrJyCxWAKJEhaTMEoX8NIWUxJJEhZKMxBIFT4XyxLwShZJ8heT83IKi1OJiAAAAAP//", "H4sIAAAAAAAAAgrJyCxWAKJEhaTMEoX8NIWUxJJEhZKMxBIFT4XyxLwShZJ8heT83IKi1OJiAAAAAP//" });
         }
 
         [Fact]
